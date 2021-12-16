0661159b246af95a0f847ed6abc513fd06f098b3
Duc Dat <phamducdat14122000@gmail.com>
2021-12-16 17:44:11 +0700

    feature/config_data

--- app/core/settings.py
+++ app/core/settings.py
@@ -60,8 +60,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'ducdat',
+        'HOST': 'db_mysql',
+        'PORT': 3306,
+        'NAME': 'ducdat',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
